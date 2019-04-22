1316151cbbb8cb9a4514eea050b0a1d4f60f608c
crod2 <carlarodriguez90@gmail.com>
2019-04-22 11:52:08 +0200

    Get images

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -17,12 +17,19 @@ function getShows() {
     .then(response => response.json())
     .then(data => {
         for (const item of data) {
+            console.log(item);
             const newItem = document.createElement('li');
             newItem.classList.add('results__item');
             const subtitle = document.createElement('h2');
             subtitle.classList.add('item__subtitle');
             const newName = document.createTextNode(item.show.name);
-            console.log(newName);
+            const img = document.createElement('img');
+            img.classList.add('item__img');
+            if (data.image === null) {
+                img.src = 'https://via.placeholder.com/250x250/';
+            } else {
+                img.src = item.show.image.medium;
+            }
         }
     })
 }
