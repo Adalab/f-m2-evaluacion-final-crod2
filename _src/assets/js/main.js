993985cd62d51de928ea72a0494508c528c73ea3
crod2 <carlarodriguez90@gmail.com>
2019-04-22 09:30:27 +0200

    Create main.js

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-console.log('>> Ready :)');
+const searchContainer = document.querySelector('.search__container');
+const title = document.querySelector('.search__container--title');
+const input = document.querySelector('.search__container--input');
+const button = document.querySelector('.search__container--button');
+const resultsContainer = document.querySelector('.results__container');
+const list = document.querySelector('.results__container--list');
+const url = 'http://api.tvmaze.com/search/shows?q='
+
+/* Al hacer clic sobre el botón de 'Buscar', nuestra aplicación debe conectarse al API abierto de TVMaze para búsqueda de series.*/
+
+function getShows() {
+    const userSearch = input.value;
+    
+    //fetch(`${url}${userSearch}`)
+}
+
+/* Para construir la URL de búsqueda necesitaremos recoger el texto que ha introducido el usuario en el campo de búsqueda. */
+
+/* Por cada show contenido en el resultado de búsqueda debemos pintar una tarjeta donde mostramos una imagen de la serie y el título. */
+
+button.addEventListener('click', getShows);
