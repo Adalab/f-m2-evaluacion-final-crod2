30951d65caba49ee9c83fcebe5be207d0898f787
crod2 <carlarodriguez90@gmail.com>
2019-04-23 11:07:09 +0200

    Minor fixes

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -6,40 +6,37 @@ const input = document.querySelector('.search__container--input');
 const button = document.querySelector('.search__container--button');
 const resultsContainer = document.querySelector('.results__container');
 const list = document.querySelector('.results__container--list');
-
-const url = 'http://api.tvmaze.com/search/shows?q='
+const url = 'http://api.tvmaze.com/search/shows?q=';
 let favShows = [];
 const favList = document.querySelector('.favs__container--list');
-
 
 function getShows() {
     const userSearch = input.value;
     list.innerHTML = '';
     fetch(`${url}${userSearch}`)
-    .then(response => response.json())
-    .then(data => {
-        for (const item of data) {
-            const newItem = document.createElement('li');
-            newItem.classList.add('results__item');
-            newItem.addEventListener('click', changeColor);
-            const subtitle = document.createElement('h2');
-            subtitle.classList.add('item__subtitle');
-            const newName = document.createTextNode(item.show.name);
-            const img = document.createElement('img');
-            img.classList.add('item__img');
-            if (item.show.image === null) {
-                img.src = 'https://via.placeholder.com/210x295/';
-            } else {
-                img.src = item.show.image.medium;
+        .then(response => response.json())
+        .then(data => {
+            for (const item of data) {
+                const newItem = document.createElement('li');
+                newItem.classList.add('results__item');
+                newItem.addEventListener('click', changeColor);
+                const subtitle = document.createElement('h2');
+                subtitle.classList.add('item__subtitle');
+                const newName = document.createTextNode(item.show.name);
+                const img = document.createElement('img');
+                img.classList.add('item__img');
+                if (item.show.image === null) {
+                    img.src = 'https://via.placeholder.com/210x295/';
+                } else {
+                    img.src = item.show.image.medium;
+                }
+                subtitle.appendChild(newName);
+                newItem.appendChild(subtitle);
+                newItem.appendChild(img);
+                list.appendChild(newItem);
             }
-            subtitle.appendChild(newName);
-            newItem.appendChild(subtitle);
-            newItem.appendChild(img);
-            list.appendChild(newItem);
-        }
-    })
+        })
 }
-
 function changeColor() {
     if (event.currentTarget.classList.contains('favourites')) {
         event.currentTarget.classList.remove('favourites');
@@ -48,7 +45,6 @@ function changeColor() {
     }
     favs(event.currentTarget);
 }
-
 function favs(triggerElement) {
     const title = triggerElement.querySelector('h2').innerHTML;
     const img = triggerElement.querySelector('img').src;
@@ -58,24 +54,22 @@ function favs(triggerElement) {
     })
     favList.innerHTML = '';
     for (const show of favShows) {
-    const newShow = document.createElement('li');
-    const favSubtitle = document.createElement('h4');
-    const favName = document.createTextNode(show.name);
-    const favImg = document.createElement('img');
-    favImg.src = show.image;
-    favSubtitle.appendChild(favName);
-    newShow.appendChild(favSubtitle);
-    newShow.appendChild(favImg);
-    favList.appendChild(newShow); 
+        const newShow = document.createElement('li');
+        const favSubtitle = document.createElement('h4');
+        const favName = document.createTextNode(show.name);
+        const favImg = document.createElement('img');
+        favImg.src = show.image;
+        favSubtitle.appendChild(favName);
+        newShow.appendChild(favSubtitle);
+        newShow.appendChild(favImg);
+        favList.appendChild(newShow);
     }
     savedData();
 }
-
 const savedData = () => {
     localStorage.setItem('saved__data', JSON.stringify(favShows));
 }
-
-const getData = () => { 
+const getData = () => {
     if (localStorage.getItem('saved__data') !== null) {
         favShows = JSON.parse(localStorage.getItem('saved__data'));
         for (const savedItem of favShows) {
@@ -87,11 +81,10 @@ const getData = () => {
             itemSubtitle.appendChild(itemName);
             newItem.appendChild(itemSubtitle);
             newItem.appendChild(itemImg);
-            favList.appendChild(newItem); 
-            }
-        
+            favList.appendChild(newItem);
+        }
+
     }
 }
-
 getData();
 button.addEventListener('click', getShows);
